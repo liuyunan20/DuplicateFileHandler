ed3fb61774771461d92349239728db64452fd769
liuyunan <liuyunan.20@gmail.com>
2022-02-03 00:19:36 -0800

    the task require an integer output

---
+++ Topics/Dictionary methods/Healthy Habits/main.py
@@ -0,0 +1,13 @@
+# the list "walks" is already defined
+# walks = [
+#     {"day": "Monday", "distance": 2000},
+#     {"day": "Tuesday", "distance": 3000},
+#     {"day": "Wednesday", "distance": 3500},
+#     {"day": "Thursday", "distance": 2500},
+#     {"day": "Friday", "distance": 2500},
+#     {"day": "Saturday", "distance": 1000},
+#     {"day": "Sunday", "distance": 5600}]
+total_distance = 0
+for walk in walks:
+    total_distance += walk.get("distance")
+print(int(total_distance / len(walks)))
