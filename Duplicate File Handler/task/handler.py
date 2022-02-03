9c235b04c95c9e32d6f6386a1fa34f9e613b6ec6
liuyunan <liuyunan.20@gmail.com>
2022-02-02 23:16:18 -0800

    merge some duplicate codes

--- Duplicate File Handler/task/handler.py
+++ Duplicate File Handler/task/handler.py
@@ -21,15 +21,12 @@ class FileHandler:
     def get_file_size(self):
         print("Enter file format:")
         file_type = input()
-        if file_type:
-            for file_path in self.files_path:
-                if os.path.splitext(file_path)[1] == file_type:
-                    size = os.path.getsize(file_path)
-                    self.files_size.setdefault(size, []).append(file_path)
-        else:
-            for file_path in self.files_path:
+
+        for file_path in self.files_path:
+            if not file_type or os.path.splitext(file_path)[1] == file_type:
                 size = os.path.getsize(file_path)
                 self.files_size.setdefault(size, []).append(file_path)
+
         for size in self.files_size:
             if len(self.files_size[size]) > 1:
                 self.dpl_size.append(size)
@@ -42,20 +39,16 @@ class FileHandler:
             print('''
 Enter a sorting option:''')
             sort_option = input()
-            if sort_option == "1":
-                for size in sorted(self.dpl_size, reverse=True):
-                    print(f"{size} bytes")
-                    print(*self.files_size[size], sep="\n")
-                    print()
-                break
-            elif sort_option == "2":
-                for size in sorted(self.dpl_size):
-                    print(f"{size} bytes")
-                    print(*self.files_size[size], sep="\n")
-                    print()
-                break
-            else:
+            if sort_option not in ["1", "2"]:
                 print("Wrong option")
+                continue
+            else:
+                descending = sort_option == "1"
+                for size in sorted(self.dpl_size, reverse=descending):
+                    print(f"{size} bytes")
+                    print(*self.files_size[size], sep="\n")
+                    print()
+                    break
 
 
 handler = FileHandler()
