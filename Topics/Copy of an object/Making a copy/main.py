b247ca4e1e375b01d3209c5cb22f7ac800cb214a
liuyunan <liuyunan.20@gmail.com>
2022-02-09 10:12:47 -0800

    copy module, deepcopy() and shallow copy, copy()

---
+++ Topics/Copy of an object/Making a copy/main.py
@@ -0,0 +1,8 @@
+import copy
+
+
+def my_copy(obj, copy_mode):
+    if copy_mode == "deep copy":
+        return copy.deepcopy(obj)
+    elif copy_mode == "shallow copy":
+        return copy.copy(obj)
